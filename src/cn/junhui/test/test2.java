955862a092a18c3cff469bbc69c0f22133f739a1
liujunhui1 <748936836@qq.com>
2019-01-26 17:22:38 +0800

    测试2

--- src/cn/junhui/test/test2.java
+++ src/cn/junhui/test/test2.java
@@ -2,10 +2,10 @@ package cn.junhui.test;
 
 /**
  * 军辉
- * 2019-01-26 17:08
+ * 2019-01-26 17:22
  */
-public class test {
+public class test2 {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test2");
     }
 }
